4b6a2744d8f1a7b43186dea001235a11bfffb19e
waqarnawaz007 <61963407+waqarnawaz007@users.noreply.github.com>
2023-07-27 15:22:58 -0500

    ccc

--- app/page/boardoftrustees/page.js
+++ app/page/boardoftrustees/page.js
@@ -8,200 +8,189 @@ export default function Home() {
                 <div class="row justify-content-center">
                     <div class="col-xl-6 col-lg-8 col-md-10">
                         <div class="section-titles text-center mb-60">
-                            <h2>BOARD <span className='text-[red]'>OF </span> TRUSTEES</h2>
+                            <h2>Board OF <span className='text-[red]'>TRUSTEES</span></h2>
                         </div>
                     </div>
                 </div>
-                <div class="row justify-content-center">
-                    <div class="col-xl-4 paddingright col-sm-3">
-                        <div class="valunteer-two-item">
-                            <div class="valunteer-two-item__img">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="our-team">
+                                <div class="pic">
                                 <Image height='150' width='150' src="/images/Prof.Dr.IntzarHussain.jpg" alt="Volunteer" />
-                            </div>
-                            <div class="valunteer-two-item__des">
-                                <h5>Chairman</h5>
-                                <span>Prof. Dr. Intzar Hussain</span>
+                                </div>
+                                <h3 class="title">Chairman</h3>
+                                <span class="post">Prof. Dr. Intzar Hussain</span>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-xl-4 paddingleftright col-sm-3">
-                        <div class="valunteer-two-item">
-                            <div class="valunteer-two-item__img">
+                
+                        <div class="col-md-3 col-sm-6">
+                            <div class="our-team">
+                                <div class="pic">
                                 <Image height='150' width='150' src="/images/ZahidLatif.jpg" alt="Volunteer" />
-                            </div>
-                            <div class="valunteer-two-item__des valunteer-two-item__des--yellow">
-                                <h5>General Secretary</h5>
-                                <span>Prof. Dr. M.Zahid Latif</span>
+                                </div>
+                                <h3 class="title">General Secretary</h3>
+                                <span class="post">Prof. Dr. M.Zahid Latif</span>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-xl-4 paddingleft col-sm-3">
-                        <div class="valunteer-two-item">
-                            <div class="valunteer-two-item__img">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="our-team">
+                                <div class="pic">
                                 <Image height='150' width='150' src="/images/Dr.ShafqatJaved.jpg" alt="Volunteer" />
-                            </div>
-                            <div class="valunteer-two-item__des valunteer-two-item__des--green">
-                                <h5>Finance Secretary</h5>
-                                <span>Dr. Shafqat Javed</span>
-                            </div>
-                        </div>
-                </div>
-                    </div>
-                <div class="row justify-content-center">
-                    <div class="col-xl-4 col-sm-6">
-                        <div class="valunteer-two-item">
-                            <div class="valunteer-two-item__img">
-                                <Image height='150' width='150' src="/images/dummyimage.jpg" alt="Volunteer" />
-                            </div>
-                            <div class="valunteer-two-item__des  valunteer-two-item__des--yellow">
-                                <h5>Member</h5>
-                                <span>Dr. Kunwar Usman Saeed</span>
+                                </div>
+                                <h3 class="title">Finance Secretary</h3>
+                                <span class="post">Dr. Shafqat Javed</span>
                             </div>
                         </div>
-                </div>
-                    <div class="col-xl-4 col-sm-6">
-                        <div class="valunteer-two-item">
-                            <div class="valunteer-two-item__img">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="our-team">
+                                <div class="pic">
                                 <Image height='150' width='150' src="/images/dummyimage.jpg" alt="Volunteer" />
-                            </div>
-                            <div class="valunteer-two-item__des  valunteer-two-item__des--green">
-                                <h5>Member</h5>
-                                <span>Dr. Tahir Farooq</span>
+                                </div>
+                                <h3 class="title">Member</h3>
+                                <span class="post">Dr. Kunwar Usman Saeed</span>
                             </div>
                         </div>
                     </div>
-                    <div class="col-xl-4 col-sm-6">
-                        <div class="valunteer-two-item">
-                            <div class="valunteer-two-item__img">
+                </div>
+                <div class="container pt-4">
+                    <div class="row justify-center">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="our-team">
+                                <div class="pic">
+                                <Image height='150' width='150' src="/images/dummyimage.jpg" alt="Volunteer" />
+                                </div>
+                                <h3 class="title">Member</h3>
+                                <span class="post">Dr. Tahir Farooq</span>
+                            </div>
+                        </div>
+                
+                        <div class="col-md-3 col-sm-6">
+                            <div class="our-team">
+                                <div class="pic">
                                 <Image height='150' width='150' src="/images/Prof.Dr.HafeezUrRahman.jpg" alt="Volunteer" />
-                            </div>
-                            <div class="valunteer-two-item__des">
-                                <h5>Member</h5>
-                                <span>Prof. Dr. Hafeez Ur Rahman</span>
+                                </div>
+                                <h3 class="title">Member</h3>
+                                <span class="post">Prof. Dr. Hafeez Ur Rahman</span>
                             </div>
                         </div>
-                    </div>
-                   </div>
-                <div class="row justify-content-center">
-                    <div class="col-xl-4 col-sm-6">
-                        <div class="valunteer-two-item">
-                            <div class="valunteer-two-item__img">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="our-team">
+                                <div class="pic">
                                 <Image height='150' width='150' src="/images/Dr.TanvirUlHasanZubairi.jpg" alt="Volunteer" />
-                            </div>
-                            <div class="valunteer-two-item__des  valunteer-two-item__des--green">
-                                <h5>Member</h5>
-                                <span>Dr. Tanvir Ul Hasan Zubairi</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-sm-6">
-                        <div class="valunteer-two-item">
-                            <div class="valunteer-two-item__img">
-                                <Image height='150' width='150' src="/images/dummyimage.jpg" alt="Volunteer" />
-                            </div>
-                            <div class="valunteer-two-item__des">
-                                <h5>Member</h5>
-                                <span>Dr. Wali Muhamamd Mujahid</span>
+                                </div>
+                                <h3 class="title">Member</h3>
+                                <span class="post">DDr. Tanvir Ul Hasan Zubairi</span>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-xl-4 col-sm-6">
-                        <div class="valunteer-two-item">
-                            <div class="valunteer-two-item__img">
-                                <Image height='150' width='150' src="/images/dummyimage.jpg" alt="Volunteer" />
-                            </div>
-                            <div class="valunteer-two-item__des valunteer-two-item__des--yellow">
-                                <h5>Member</h5>
-                                <span>Dr. Ahmad Khan</span>
-                            </div>
-                        </div>
+                        
                     </div>
                 </div>
-                   
+                <div class="container pt-4">
+                    <div class="row justify-center">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="our-team">
+                                <div class="pic">
+                                <Image height='150' width='150' src="/images/dummyimage.jpg" alt="Volunteer" />
+                                </div>
+                                <h3 class="title">Member</h3>
+                                <span class="post">Dr. Wali Muhamamd Mujahid</span>
+                            </div>
+                        </div>
+                
+                        <div class="col-md-3 col-sm-6">
+                            <div class="our-team">
+                                <div class="pic">
+                                <Image height='150' width='150' src="/images/dummyimage.jpg" alt="Volunteer" />
+                                </div>
+                                <h3 class="title">Member</h3>
+                                <span class="post">Dr. Ahmad Khan</span>
+                            </div>
+                        </div>
+                      
+                    </div>
+                </div>                   
             </div>
         </div>       
-        <div class="volunteer-style-two py-120 pb-90 rel z-1 ">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-xl-6 col-lg-8 col-md-10">
-                        <div class="section-titleS text-center mb-60">
-                            <h2>NATIONAL <span className='text-[#ED1C24]'>WORKING </span>COUNCIL</h2>
-                        </div>
+        <div class="volunteer-style-three pb-90 rel z-1">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-6 col-lg-8 col-md-10">
+                    <div class="section-titleS text-center mb-60">
+                        <h2>NATIONAL <span className='text-[#ED1C24]'>WORKING </span>COUNCIL</h2>
                     </div>
                 </div>
-                <div class="row justify-content-center">
-                    <div class="col-xl-3 col-sm-6">
-                        <div class="valunteer-two-item">
-                            <div class="valunteer-two-item__img">
-                                <Image height='150' width='150' src="/images/Prof.Dr.IntzarHussain.jpg" alt="Volunteer" />
-                            </div>
-                            <div class="valunteer-two-item__des">
-                                <h5>Member</h5>
-                                <span>Prof. Dr. Intzar Hussain</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-sm-6">
-                        <div class="valunteer-two-item">
-                            <div class="valunteer-two-item__img">
-                                <Image height='150' width='150' src="/images/ZahidLatif.jpg" alt="Volunteer" />
-                            </div>
-                            <div class="valunteer-two-item__des valunteer-two-item__des--yellow">
-                                <h5>Member</h5>
-                                <span>Dr Misbah ul Aziz</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-sm-6">
-                        <div class="valunteer-two-item">
-                            <div class="valunteer-two-item__img">
-                                <Image height='150' width='150' src="/images/Dr.ShafqatJaved.jpg" alt="Volunteer" />
-                            </div>
-                            <div class="valunteer-two-item__des valunteer-two-item__des--green">
-                                <h5>Member</h5>
-                                <span>Prof. Dr. Muhammad Zahid Latif</span>
-                            </div>
-                        </div>
-                </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-xl-3 col-sm-6">
-                        <div class="valunteer-two-item">
-                            <div class="valunteer-two-item__img">
-                                <Image height='150' width='150' src="/images/dummyimage.jpg" alt="Volunteer" />
-                            </div>
-                            <div class="valunteer-two-item__des  valunteer-two-item__des--yellow">
-                                <h5>Member</h5>
-                                <span>Dr Shayan Shadmani</span>
-                            </div>
-                        </div>
-                </div>
-                    <div class="col-xl-3 col-sm-6">
-                        <div class="valunteer-two-item">
-                            <div class="valunteer-two-item__img">
-                                <Image height='150' width='150' src="/images/dummyimage.jpg" alt="Volunteer" />
-                            </div>
-                            <div class="valunteer-two-item__des  valunteer-two-item__des--green">
-                                <h5>Member</h5>
-                                <span>Dr. Tahir Farooq</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-sm-6">
-                        <div class="valunteer-two-item">
-                            <div class="valunteer-two-item__img">
-                                <Image height='150' width='150' src="/images/Prof.Dr.HafeezUrRahman.jpg" alt="Volunteer" />
-                            </div>
-                            <div class="valunteer-two-item__des">
-                                <h5>Member</h5>
-                                <span>Dr. Naseer Ahmad Safi</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                   
             </div>
-        </div> 
+            <div class="row justify-content-center">
+                <div class="col-xl-3 col-sm-6">
+                    <div class="valunteer-item valunteer-item--three">
+                        <div class="valunteer-item__img">
+                        <Image height='150' width='150' src="/images/Prof.Dr.IntzarHussain.jpg" alt="Volunteer" />
+                        </div>
+                        <div class="valunteer-item__designation">
+                            <h5>Member</h5>
+                            <span>Prof. Dr. Intzar Hussain</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-6">
+                    <div class="valunteer-item valunteer-item--three">
+                        <div class="valunteer-item__img">
+                            <Image height='150' width='150' src="/images/ZahidLatif.jpg" alt="Volunteer" />
+                        </div>
+                        <div class="valunteer-item__designation">
+                            <h5>Member</h5>
+                            <span>Dr Misbah ul Aziz</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-6">
+                    <div class="valunteer-item valunteer-item--three">
+                        <div class="valunteer-item__img">
+                        <Image height='150' width='150' src="/images/Dr.ShafqatJaved.jpg" alt="Volunteer" />
+                        </div>
+                        <div class="valunteer-item__designation">
+                            <h5>Member</h5>
+                            <span>Prof. Dr. Muhammad Zahid Latif</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-6">
+                    <div class="valunteer-item valunteer-item--three">
+                        <div class="valunteer-item__img">
+                        <Image height='150' width='150' src="/images/dummyimage.jpg" alt="Volunteer" />
+                        </div>
+                        <div class="valunteer-item__designation">
+                            <h5>Member</h5>
+                            <span>Dr Shayan Shadmani</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-6">
+                    <div class="valunteer-item valunteer-item--three">
+                        <div class="valunteer-item__img">
+                        <Image height='150' width='150' src="/images/dummyimage.jpg" alt="Volunteer" />
+                        </div>
+                        <div class="valunteer-item__designation">
+                            <h5>Member</h5>
+                            <span>Dr. Tahir Farooq</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-6">
+                    <div class="valunteer-item valunteer-item--three">
+                        <div class="valunteer-item__img">
+                        <Image height='150' width='150' src="/images/Prof.Dr.HafeezUrRahman.jpg" alt="Volunteer" />
+                        </div>
+                        <div class="valunteer-item__designation">
+                            <h5>Member</h5>
+                            <span>Dr. Naseer Ahmad Safi</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
         </>
     )
 }
